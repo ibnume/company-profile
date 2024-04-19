cf654e2aaa8e41dbbbe8751ff96857ec3b505f60
ibnume <160526908+ibnume@users.noreply.github.com>
2024-04-20 02:04:31 +0700

    aa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Hero from "./components/Ul/Hero";
+import Counter from "./components/Ul/Counter";
+import Services from "./components/Ul/Services";
+import About from "./components/Ul/About";
+import Team from "./components/Ul/Team";
+import Blog from "./components/Ul/Blog";
+import Testimonial from "./components/Ul/Testimonial";
+import Newsletter from "./components/Ul/Newsletter";
+import Footer from "./components/Footer/Footer";
 
 function App() {
+  const [theme, setTheme] = useState("");
+
+  const toggleTheme = () => {
+    theme === "" ? setTheme("light-theme") : setTheme("");
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header theme={theme} toggleTheme={toggleTheme} />
+      <Hero theme={theme} />
+      <Counter />
+      <Services />
+      <About />
+      <Team />
+      <Blog />
+      <Testimonial />
+      <Newsletter />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Ul/Team.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "../../styles/team.css";
+
+import team01 from "../../images/team-01.png";
+import team02 from "../../images/team-02.png";
+import team03 from "../../images/team-03.png";
+
+const teamMembers = [
+  {
+    imgUrl: team01,
+    name: "Courtny Hurry",
+    position: "Product Developer",
+  },
+  {
+    imgUrl: team02,
+    name: "Lindas Walton",
+    position: "Front-End Developer",
+  },
+  {
+    imgUrl: team03,
+    name: "Harry Martin",
+    position: "Product Designer",
+  },
+  {
+    imgUrl: team02,
+    name: "Lisa Cooper",
+    position: "CEO & Sr Developer",
+  },
+];
+
+const Team = () => {
+  return (
+    <section className="our__team">
+      <div className="container">
+        <div className="team__content">
+          <h6 className="subtitle">Our Team</h6>
+          <h2>
+            Meet with <span className="highlight">our team</span>
+          </h2>
+        </div>
+
+        <div className="team__wrapper">
+          {teamMembers.map((item, index) => (
+            <div className="team__item" key={index}>
+              <div className="team__img">
+                <img src={item.imgUrl} alt="" />
+              </div>
+              <div className="team__details">
+                <h4>{item.name}</h4>
+                <p className="description">{item.position}</p>
+
+                <div className="team__member-social">
+                  <span>
+                    <i class="ri-linkedin-line"></i>
+                  </span>
+                  <span>
+                    <i class="ri-twitter-line"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
